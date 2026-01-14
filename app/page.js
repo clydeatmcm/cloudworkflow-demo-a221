3019dcffa49f0964c6bb3a384a4928690ab7050d
clydeatmcm <ccrbalaman@mcm.edu.ph>
2026-01-14 03:54:26 +0000

    next js project boilerplate

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold">Pokémon API Demo</h1>
+    </main>
+  );
+}
